c5ab83c161cf837252893e94ecd25f8a0ebe9959
TINH DANG <dangductinh1103@gmail.com>
2025-06-02 21:19:17 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'utils/currency_formatter.dart';
+import 'viewmodels/auth_viewmodel.dart';
+import 'viewmodels/expense_viewmodel.dart';
+import 'viewmodels/calendar_viewmodel.dart';
+import 'viewmodels/report_viewmodel.dart';
+import 'viewmodels/search_viewmodel.dart';
+import 'viewmodels/more_viewmodel.dart';
+import 'views/screens/splash_screen.dart';
+import 'views/screens/login_screen.dart';
+import 'views/screens/signup_screen.dart';
+import 'views/screens/expense_screen.dart';
+import 'views/screens/calendar_screen.dart';
+import 'views/screens/report_screen.dart';
+import 'views/screens/more_screen.dart';
+import 'views/screens/search_screen.dart';
+import 'views/screens/forgot_password_screen.dart';
+import 'localization/app_localizations.dart';
+import 'providers/locale_provider.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // Ensure Flutter is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
+  // Initialize currency settings
+  await initCurrency();
+
+  // Load the saved locale before running the app
+  final savedLocale = await AppLocalizations.getLocale();
+
+  // Run the application with the saved locale
+  runApp(FundyApp(initialLocale: savedLocale));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FundyApp extends StatefulWidget {
+  final Locale initialLocale;
 
-  // This widget is the root of your application.
+  const FundyApp({Key? key, required this.initialLocale}) : super(key: key);
+
+  @override
+  _FundyAppState createState() => _FundyAppState();
+}
+
+class _FundyAppState extends State<FundyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewModel()),
+        ChangeNotifierProvider(create: (_) => ExpenseViewModel()),
+        ChangeNotifierProvider(create: (_) => CalendarViewModel()),
+        ChangeNotifierProvider(create: (_) => ReportViewModel()),
+        ChangeNotifierProvider(create: (_) => SearchViewModel()),
+        ChangeNotifierProvider(create: (_) => MoreViewModel()),
+        ChangeNotifierProvider(
+            create: (_) => LocaleProvider(initialLocale: widget.initialLocale)),
+      ],
+      child: Consumer<LocaleProvider>(
+        builder: (context, localeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Fundy',
+            // Cấu hình đa ngôn ngữ
+            locale: localeProvider.locale,
+            supportedLocales: AppLocalizations.supportedLocales,
+            localizationsDelegates: [
+              const AppLocalizationsDelegate(),
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            home: ExitConfirmationWrapper(child: SplashScreen()),
+            routes: {
+              '/login': (context) => ExitConfirmationWrapper(child: LoginScreen()),
+              '/signup': (context) => ExitConfirmationWrapper(child: SignupScreen()),
+              '/expense': (context) => ExitConfirmationWrapper(child: ExpenseScreen()),
+              '/calendar': (context) => ExitConfirmationWrapper(child: CalendarScreen()),
+              '/report': (context) => ExitConfirmationWrapper(child: ReportScreen()),
+              '/more': (context) => ExitConfirmationWrapper(child: MoreScreen()),
+              '/search': (context) => SearchScreen(), // Không thêm xác nhận thoát cho màn hình tìm kiếm
+              '/forgot_password': (context) => ExitConfirmationWrapper(child: ForgotPasswordScreen()),
+            },
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -130,7 +130,7 @@ class DatabaseService {
     required DateTime date,
     required bool isExpense,
   }) async {
-    if (currentUserId == null) throw Exception('User not logged in');
+    if (currentUserId == null) throw Exception('User not logged in');t
 
     ExpenseModel expense = ExpenseModel(
       id: '',
