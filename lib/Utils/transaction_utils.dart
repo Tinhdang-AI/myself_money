637fc4c5a2eedfb819482ecf73e76dea0da6164d
Mugen1203 <2251120236@ut.edu.vn>
2025-06-03 08:23:05 +0700

    update

--- lib/Utils/transaction_utils.dart
+++ lib/Utils/transaction_utils.dart
@@ -82,3 +82,112 @@ class TransactionUtils {
       },
     );
   }
+  
+  // Show edit dialog
+  static Future<EditResult?> showEditDialog(
+      BuildContext context,
+      ExpenseModel expense,
+      List<Map<String, dynamic>> categoryList) {
+    final TextEditingController noteController = TextEditingController(text: expense.note);
+
+    // Convert the amount from VND to current currency
+    final double convertedAmount = convertFromVND(expense.amount);
+    final TextEditingController amountController = TextEditingController(
+        text: getCurrentCurrencyFormatter().format(convertedAmount));
+
+
+    // Keep original date value
+    DateTime selectedDate = expense.date;
+
+    // Initialize selected category and category icon
+    String selectedCategory = expense.category;
+    String selectedCategoryIcon = expense.categoryIcon;
+
+    // Format date as dd/MM/yyyy
+    final dateController = TextEditingController(
+        text: DateFormat('dd/MM/yyyy').format(selectedDate));
+
+
+    return showDialog<EditResult?>(
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setState) {
+          return Theme(
+            // Apply a custom theme for this dialog
+            data: Theme.of(context).copyWith(
+              textSelectionTheme: TextSelectionThemeData(
+                cursorColor: Colors.orange,
+                selectionColor: Colors.orange.withOpacity(0.3),
+                selectionHandleColor: Colors.orange,
+              ),
+            ),
+            child: AlertDialog(
+              title: Text(context.tr('edit_transaction')),
+              backgroundColor: Colors.white,
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Category selection section
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('${context.tr('category')}:', style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 8),
+                        // Tappable container to open category selection dialog
+                        GestureDetector(
+                          onTap: () {
+                            // Show category selection dialog
+                            _showCategorySelectionDialog(
+                                context,
+                                categoryList,
+                                expense.isExpense,
+                                selectedCategory,
+                                    (category, icon) {
+                                  setState(() {
+                                    selectedCategory = category;
+                                    selectedCategoryIcon = icon;
+                                  });
+                                }
+                            );
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade100,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: Colors.black, width: 1.0),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // Display selected category with overflow handling
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        IconData(int.parse(selectedCategoryIcon), fontFamily: 'MaterialIcons'),
+                                        color: Colors.orange,
+                                      ),
+                                      SizedBox(width: 8),
+                                      Flexible(
+                                        child: Text(
+                                          selectedCategory,
+                                          style: TextStyle(fontSize: 16, color: Colors.black),
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 1,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                // Add icon to indicate it's tappable
+                                Icon(Icons.arrow_drop_down, color: Colors.black),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
